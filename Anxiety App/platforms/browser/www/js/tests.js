71aad747e1ff5cb57d082fc2faf9c9acf52810d6
myusernamewastaken <mikolajm@kean.edu>
2016-11-01 18:13:06 -0400

    Added Kean counseling center Phone# to resource page, added QUnit framework
    The QUnit framework is just the base template (testing if it works), we
    still need to write the tests.
    
    Added Phone # to resource page as requested.

---
+++ Anxiety App/platforms/browser/www/js/tests.js
@@ -0,0 +1,14 @@
+
+
+/* This file should contain the automated Tests
+*  for the Anxiety app.  These tests are run using QUnit.
+*  For Usage, please see the "cookbook" at: https://qunitjs.com/cookbook/ */
+
+
+/* Test to ensure QUnit is running.
+* If this test fails, we have bigger issues. */
+QUnit.test( "Sanity Test", function( assert ) {
+
+  assert.ok( true, "QUnit is running." );
+
+});
